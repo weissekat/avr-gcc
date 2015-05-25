7ffa259b793046af3a2155df467c703cff793331
weissekat <weissekat@gmail.com>
2015-05-25 11:05:46 +0300

    Whitespace fix

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 volatile int workmode;
 
 //ISR(PCINT0_vect) {
-//	workmode ^= 1 << 0;
+//  workmode ^= 1 << 0;
 //};
 
 void hush(void) {
@@ -15,25 +15,25 @@ void work(void) {
 };
 
 void init(void) {
-//	cli();
-//	GIMSK |= 1 << PCIE;
-//	PCMSK |= 1 << PCINT0;
-//    MCUCR &= ~((1<<ISC01) | (1<<ISC00));
-//    MCUCR |= (1<<ISC01) | (1<<ISC00);
-//	sei();
-//	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	workmode = 1;
+//  cli();
+//  GIMSK |= 1 << PCIE;
+//  PCMSK |= 1 << PCINT0;
+//  MCUCR &= ~((1<<ISC01) | (1<<ISC00));
+//  MCUCR |= (1<<ISC01) | (1<<ISC00);
+//  sei();
+//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    workmode = 1;
 };
 
 int main (void) {
-	init();
+    init();
     while(1) {
-  		if (workmode == 1) {
+        if (workmode == 1) {
             work();
-  		} else {
-  			hush();
-  	        sleep_enable();
-          	sleep_cpu();
-  		};
+        } else {
+            hush();
+            sleep_enable();
+            sleep_cpu();
+        };
     };
 };
